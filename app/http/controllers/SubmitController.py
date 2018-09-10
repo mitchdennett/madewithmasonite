388efa54fe07e103e36499b0a2a8f8246987b69c
Mitchell Dennett <mitch@mylodevelopment.com>
2018-09-10 15:22:46 -0400

    Added Submit COntroller

---
+++ app/http/controllers/SubmitController.py
@@ -0,0 +1,7 @@
+''' A Module Description '''
+
+class SubmitController:
+    ''' Class Docstring Description '''
+
+    def show(self):
+        pass
--- routes/web.py
+++ routes/web.py
@@ -3,4 +3,5 @@ from masonite.routes import Get, Post
 
 ROUTES = [
     Get().route('/', 'WelcomeController@show').name('welcome'),
+    Get().route('/submit', 'SubmitController@show').name('submit')
 ]
